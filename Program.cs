34ed169775afd7a4fb5fa83fccc26207ee3d1c76
AnushaPoosala <117409595+AnushaPoosala@users.noreply.github.com>
2024-04-02 17:20:07 -0400

    Insert element at given position into LinkedList using Recursion

--- Program.cs
+++ Program.cs
@@ -1,62 +1,73 @@
 ﻿using System;
-//https://www.youtube.com/watch?v=dBBm5pltWe4&list=PLt4nG7RVVk1gDMcCZkpjOxZS4XMro29DU
-//Creation of linked list using Recursion
-//questions you should answer 
-//1.Why we are creating object in GetNewNode Method why not just after LinkedList(before insert methode)
-//2.how we are able to access root.next in Insert,Print methods without creating instance to Node.
-//3. nullHandlingException, objectReference is not set to an instance exception
-namespace Stack
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//https://www.youtube.com/watch?v=_VWBbcl_EYA&list=PLt4nG7RVVk1gDMcCZkpjOxZS4XMro29DU&index=6
+//4 - Insert element at given position in LinkedList
+namespace _6InsertElementAtGivenPositionUsingRecursion
 {
-    class Node
+    internal class Program
     {
-        public int data;
-        public Node next;
-    }
-    class LinkedList
-    {
-        public Node Insert(int ele, Node root)
+        public class Node
         {
-            if (root == null)
-                return GetNewNode(ele, root);
-            else
-                root.next = Insert(ele, root.next);
-            return root;
+            public int Data { get; set; }
+            public Node Next { get; set; }
         }
-        public Node GetNewNode(int ele, Node root)
+        public class LinkedList
         {
-            Node node = new Node { data = ele, next = null };
-            return node;
-        }
-        public void Print(Node root)
-        {
-            if (root == null)
-                return;
-            else
+            public Node InsertLast(int data,Node root)
             {
-                Console.Write("{0}  ", root.data);
-                Print(root.next);
+                if (root == null)
+                    return NewNode(data);
+                else
+                {
+                    root.Next = InsertLast(data, root.Next);
+                    return root;
+                }
             }
-            Console.WriteLine();
+            public Node NewNode(int data)
+            {
+                Node node=new Node() {  Data = data,Next=null  };
+                return node;
+            }
+            public void Print(Node root)
+            {
+                if (root == null) return;
+                else
+                {
+                    Console.WriteLine(root.Data);
+                    Print(root.Next);
+                }
+                Console.WriteLine();
+            }
+
+            public Node InsertAtPositionUsingRecursion(int position, int data, Node node)
+            {
+                if( position<1)
+                    Console.WriteLine("Position can not be less than 1: It is Invalid");
+                if(node==null && position>1)
+                    Console.WriteLine("When node is null we can insert only at position1: Invalid Position");
+                if (node == null && position == 1) return NewNode(data);
+                if(node!=null && position == 1) { Node newNode = NewNode(data); newNode.Next = node; return newNode; }
+
+                node.Next = InsertAtPositionUsingRecursion(position-1, data, node.Next);
+                return node;
+            }
         }
-    }
-
-
-    class StackLinkedListImplementation
-    {
         static void Main(string[] args)
         {
-            Node root = null;
-            LinkedList lL = new LinkedList();
-            root = lL.Insert(12, root);
-            root = lL.Insert(13, root);
-            root = lL.Insert(14, root);
-            root = lL.Insert(15, root);
+            Node node = null;
+            LinkedList ll=new LinkedList();
+            node=ll.InsertLast(1, node);
+            node = ll.InsertLast(2, node);
+            node = ll.InsertLast(3, node);
+            node = ll.InsertLast(5, node);
+            node = ll.InsertLast(6, node);
+            node = ll.InsertLast(7, node);
+            ll.Print(node);
 
-            lL.Print(root);
-
-            root = lL.Insert(16, root);
-            lL.Print(root);
-
+            node = ll.InsertAtPositionUsingRecursion(4, 4, node);
+            ll.Print(node);
         }
     }
 }
